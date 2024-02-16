3a19bd89e95289c26cfa955a912188e65e4f61e7
AbduHany <abdu.hany@gmail.com>
2024-02-16 22:49:41 +0200

    Modify 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -33,15 +33,19 @@ void shell_sort(int *array, size_t size)
 		return;
 	while ((maxgap * 3 + 1) <= (int)size - 1)
 		maxgap = (maxgap * 3) + 1;
+	/* Maxgap intervals done through Knuth sequence */
 	for (; maxgap > 0; maxgap = (maxgap - 1) / 3)
 	{
 		for (i = 0; i + maxgap < (int)size; i++)
 		{
 			if (array[i] > array[i + maxgap])
 			{
+				/* Swap the forward element */
 				swap(i, i + maxgap, array);
+				/* Check for backward elements */
 				for (k = i, j = i - maxgap; j >= 0; j -= maxgap, k -= maxgap)
 				{
+					/* Swap backward elements */
 					if (array[k] < array[j])
 						swap(k, j, array);
 					else
