0e57d27603407fcca62893682657c2f1dbfe1e79
AbduHany <abdu.hany@gmail.com>
2024-02-16 14:59:37 +0200

    Add 3-quick_sort.c

---
+++ 3-quick_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * quick_sort - this function sorts an array using the
+ * quick sort algorithm.
+ * @array: int pointer to the array to be sorted.
+ * @size: the size of the array to be sorted.
+ *
+ * Return: void.
+ */
+void quick_sort(int *array, size_t size)
+{
+	int pivot = array[size - 1], i, j;
+
+}
