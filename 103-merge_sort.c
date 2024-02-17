2d400584f54c0341a27fbcfd117054f8d5a28710
AbduHany <abdu.hany@gmail.com>
2024-02-17 14:37:27 +0200

    Add 103-merge_sort.c

---
+++ 103-merge_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * merge_sort - sorts an array using the merge sort algorithm.
+ * @array: int pointer to the array being sorted.
+ * @size: size of the array being sorted.
+ *
+ * Return: void.
+ */
+void merge_sort(int *array, size_t size)
+{
+	(void)array;
+	(void)size;
+}
