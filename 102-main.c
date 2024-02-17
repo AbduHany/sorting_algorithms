7fdfced6ff7a869f1cbad5d533f2a6af329ba2ef
AbduHany <abdu.hany@gmail.com>
2024-02-17 06:16:59 +0200

    Add 102-counting_sort.c

--- 102-main.c
+++ 102-main.c
@@ -9,13 +9,12 @@
  */
 int main(void)
 {
-	int array[] = {27, 14, 4, 21, 2, 20, 8, 24, 3, 5, 19, 11,
-		15, 29, 28, 25, 18, 17, 30, 16};
+	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
 	size_t n = sizeof(array) / sizeof(array[0]);
 
 	print_array(array, n);
 	printf("\n");
-	selection_sort(array, n);
+	counting_sort(array, n);
 	printf("\n");
 	print_array(array, n);
 	return (0);
